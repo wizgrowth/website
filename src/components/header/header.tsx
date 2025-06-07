1cfdec590c8735f236818d038bd60fd1a415eae0
NikhilJosephK <nikhilnjk@gmail.com>
2025-06-08 02:08:36 +0530

    feat: update Blog link active state to support nested routes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -63,7 +63,7 @@ export function Header() {
             </a>
             <a
               href="/blog/"
-              className={`font-medium hover:bg-primary-100 px-3 py-2 rounded-md ${pathName === '/blog/' ? 'bg-primary-100' : ''}`}
+              className={`font-medium hover:bg-primary-100 px-3 py-2 rounded-md ${pathName.startsWith('/blog/') ? 'bg-primary-100' : ''}`}
             >
               Blog
             </a>

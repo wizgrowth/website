2e8d493cff4c2a2d807e9da49407843686bc5a2b
NikhilJosephK <nikhilnjk@gmail.com>
2025-06-13 10:53:19 +0530

    refactor(robots): update disallow rules and comment out sitemap, deleted sitemap until nameserver is updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,13 @@
+// robots disallowed and sitemap commented until name server is changed
+
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      disallow: '/',
     },
-    sitemap: `${process.env.SITE_DOMAIN}/sitemap.xml`,
+    // sitemap: `${process.env.SITE_DOMAIN}/sitemap.xml`,
   }
 }

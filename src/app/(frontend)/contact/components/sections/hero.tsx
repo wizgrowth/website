06082dc14dde452a728a3fb8a9f0d01c7996f8dc
NikhilJosephK <nikhilnjk@gmail.com>
2025-07-05 17:05:49 +0530

    fix: update image sources to use new Supabase URLs

--- src/app/(frontend)/contact/components/sections/hero.tsx
+++ src/app/(frontend)/contact/components/sections/hero.tsx
@@ -18,7 +18,7 @@ export function Hero() {
             />
           </div>
           <Image
-            src="https://vdskmkiggnujcnwluksm.supabase.co/storage/v1/object/sign/wizgrowth-assets/contact/contact-page-hero-image.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5XzMzOWM5YzU5LTM3MmEtNGQ3NC1hOWU0LWVlMDY5ZTdhYjRlYSJ9.eyJ1cmwiOiJ3aXpncm93dGgtYXNzZXRzL2NvbnRhY3QvY29udGFjdC1wYWdlLWhlcm8taW1hZ2UucG5nIiwiaWF0IjoxNzQ2NjMwMjcxLCJleHAiOjIwNjE5OTAyNzF9.8u4Hbe6Jg_XTK_61Wi51vWHOwQpDXtBX7C1PlnHrf2g"
+            src="https://ibffbzwoucksfljolszp.supabase.co/storage/v1/object/public/wizgrowth-assets/contact-us-page/contact-page-hero-image.png"
             alt="Hero Image"
             width={443}
             height={407}
